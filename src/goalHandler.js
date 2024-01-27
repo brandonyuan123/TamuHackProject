e8a05e41b40d2283591c3c39766a6baced75f340
Alexander <Tankerbox@comcast.net>
2024-01-27 17:36:26 -0600

    goal object

--- src/goalHandler.js
+++ src/goalHandler.js
@@ -1,32 +1,54 @@
 import FinancialGoal from './goalObject.js';
-
+import fs from 'fs';
 class goalHandler {
-    constructor() {
-        this.goals = [];
+    constructor(filename = 'financialGoalData.json') {
+        this.filename = filename
+        this.goals = this.readFromFile();
+    }
+
+    readFromFile() {
+        try {
+            const data = fs.readFileSync(this.filename, 'utf-8')
+            return JSON.parse(data)
+        } catch (error) {
+            return [];
+        }
+    }
+
+    writeToFile() {
+        try {
+            fs.writeFileSync(this.filename, JSON.stringify(this.goals, null, 3));
+        } catch (error) {
+            console.error('Error writing file:', error);
+        }
     }
 
     addGoal(goalAmount, savedAmount, url, name) {
-        const newGoal = new FinancialGoal();
-        newGoal.setGoal(goalAmount, savedAmount, url, name);
+        const newGoal = new FinancialGoal(goalAmount, savedAmount, url, name);
         this.goals.push(newGoal);
+        this.writeToFile();
     }
 
     updateGoal(index, addedAmount) {
-        if (this.goals[index]) {
-            this.goals[index].updateGoal(addedAmount);
+        if (index >= 0 && index < this.goals.length) {
+            this.goals[index].updateSavedAmount(addedAmount);
+            this.writeToFile();
+            return this.goals[index];
         } else {
-            console.log('Goal not found at index:', index);
+            throw new Error('Goal not found at index: ' + index);
         }
     }
 
     getGoal(index) {
-        if (index < this.goals.length) {
-            return {goalData: this.goals[index].goalData}
+        if (index >= 0 && index < this.goals.length) {
+            return this.goals[index];
+        } else {
+            throw new Error('Goal not found at index: ' + index);
         }
     }
 
     getAllGoals() {
-        return this.goals.map(goal => goal.goalData)
+        return this.goals;
     }
 }
 
--- src/goalObject.js
+++ src/goalObject.js
@@ -1,52 +1,15 @@
-import fs from 'fs';
-
-class goalObject {
-    constructor(filename = 'financialGoalData.json') {
-        this.filename = filename
+class FinancialGoal {
+    constructor(goalAmount, savedAmount, url, name) {
+        this.goalAmount = goalAmount;
+        this.savedAmount = savedAmount;
+        this.url = url;
+        this.name = name;
     }
 
-    setGoal(goalAmount, savedAmount, url, name) {
-        this.goalData = {
-            goalAmount,
-            savedAmount,
-            url,
-            name,
-        };
-
-        this.writeToFile();
-    }
-
-    updateGoal(addedAmount) {
-        this.readFromFile(() => {
-            this.goalData.savedAmount += addedAmount;
-            this.writeToFile(true); // Update message
-        });
-    }
-
-    readFromFile(callback) {
-        fs.readFile(this.filename, 'utf8', (err, data) => {
-            if (err) {
-                console.log('Could not read file:', err);
-            } else {
-                this.goalData = JSON.parse(data);
-                callback();
-            }
-        });
-    }
-
-    writeToFile(updateMessage = false) {
-        fs.writeFile(this.filename, JSON.stringify(this.goalData, null, 3), (err) => {
-            if (err) {
-                console.log('Could not write to file:', err);
-            } else {
-                if (updateMessage) {
-                    console.log('Updated Successfully:', this.goalData.savedAmount, this.goalData.completed);
-                } else {
-                    console.log('Financial goal saved successfully.');
-                }
-            }
-        });
+    updateSavedAmount(addedAmount) {
+        this.savedAmount += addedAmount;
+        this.completed = this.savedAmount >= this.goalAmount;
     }
 }
 
-export default goalObject;
+export default FinancialGoal;
