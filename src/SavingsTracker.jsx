fb2963149b11b1f8178c22929c9a02b86d63211d
Brandon Yuan <brandonyuan05@gmail.com>
2024-01-27 19:24:10 -0600

    print 2d list of goals -> attribute of goals

--- src/SavingsTracker.jsx
+++ src/SavingsTracker.jsx
@@ -2,8 +2,16 @@ import {createHandler} from './goalManager.js'
 
 export default function SavingsTracker() {
     var goalHandler = createHandler()
+
     goalHandler.addGoal(400, 10, 'http://amazon.com', 'Graphics Card');
-    var goal = goalHandler.getGoal(0)
+    goalHandler.addGoal(1, 10, 'http://amazon.org', 'Graphics Card');
+    goalHandler.addGoal(400, 20, 'http://amazon.com', 'Graphics CardTTESTESTESTE');
+    goalHandler.addGoal(99999, 10, 'http://amazon.com', 'Graphics Card');
+    goalHandler.addGoal(0, 10, 'http://amazon.comTESTESTSET', 'Graphics Card');
+    goalHandler.addGoal(400, 120, 'http://amazon.com', 'Graphics Card');
+
+    //var goal = goalHandler.getGoal(0)
+    var goalsList = goalHandler.getAllGoals()
     return (
         <>
         <div>
@@ -12,8 +20,12 @@ export default function SavingsTracker() {
         </div>
         <div>
             <ul>
-                {goal.map((item, index) => (
-                    <li key={index}>{item}</li>
+                {goalsList.map((goal, index) => (
+                    <div key={index}>
+                        {goal.map((item, index2) => (
+                            <li key={index2}>{item}</li>
+                        ))}
+                    </div>
                 ))}
             </ul>
         </div>
