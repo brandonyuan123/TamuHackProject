e5a73ed146eac3cc1147ec5c1d4867f66cea91dc
Alexander <Tankerbox@comcast.net>
2024-01-27 18:11:33 -0600

    goal object

--- src/goalManager.js
+++ src/goalManager.js
@@ -1,7 +1,16 @@
 import goalHandler from './goalHandler.js';
 
-const goalManager = new goalHandler();
+let goalManager = null
+export function createHandler(){
+    goalManager = new goalHandler();
+}
 
+export function addGoal(goalAmount, savedAmount, url, name) {
+    goalManager.addGoal(goalAmount, savedAmount, url, name);
+}
+
+
+/*
 goalManager.addGoal(400, 10, 'http://amazon.com', 'Graphics Card');
 goalManager.addGoal(200, 100, 'http://amazon.com', 'SSD');
 
@@ -13,3 +22,4 @@ goalManager.updateGoal(0, 400)
 console.log(goalManager.getGoal(0))
 console.log(goalManager.getPercent(0))
 console.log(goalManager.getGoalComplete(0))
+ */
