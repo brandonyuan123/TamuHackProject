f340fdc76ccc2f9ba017fe48191f08cc220ba1e5
Brandon Yuan <brandonyuan05@gmail.com>
2024-01-27 19:01:06 -0600

    rendering the list of attributes of goal

---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+goalManager.updateGoal(0, 200)
+// console.log(goalManager.getGoal(0))
+// console.log(goalManager.getPercent(0))
+// console.log(goalManager.getGoalComplete(0))
+// goalManager.updateGoal(0, 400)
+// console.log(goalManager.getGoal(0))
+// console.log(goalManager.getPercent(0))
+// console.log(goalManager.getGoalComplete(0))
